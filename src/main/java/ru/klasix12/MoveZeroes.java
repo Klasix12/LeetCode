cf0c44ab019959136a3bdc910ac1c4b37c218962
Artem Oleshev <sunlightpattern@yandex.ru>
2025-05-07 14:19:53 +0300

    move zeroes

---
+++ src/main/java/ru/klasix12/MoveZeroes.java
@@ -0,0 +1,24 @@
+package ru.klasix12;
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        int n = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[n] = nums[i];
+                n++;
+            }
+        }
+        for (int i = n; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void main(String[] args) {
+        moveZeroes(new int[]{0, 1, 0, 3, 12});
+        moveZeroes(new int[]{0, 1});
+    }
+}
